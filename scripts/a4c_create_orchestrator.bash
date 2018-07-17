3043891f71183b7dd0f7fc4e06db3678b6c88a87
Laurent Ganne <ganne.laurent@gmail.com>
2018-07-17 16:02:16 +0000

    Updated scripts

--- scripts/a4c_create_orchestrator.bash
+++ scripts/a4c_create_orchestrator.bash
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 #
-# a4c_upload_yorc_plugin.bash - uploads Yorc plugin in Alien4Cloud
+# a4c_create_orchestrator.bash - creates a Yorc orchestrator in Alien4Cloud
+# Prerequisite: the Yorc Alien4Cloud plugin was registered in Alien4Cloud
+#               (see a4c_upload_plugin.bash)
 #
 
 a4cURL="http://localhost:8088"
-yorcPluginURL="https://github.com/ystia/yorc-a4c-plugin/releases/download/v3.0.0/alien4cloud-yorc-plugin-3.0.0.zip"
+yorcURL="http://localhost:8800"
 usage() {
     echo ""
     echo "Usage:"
-    echo "a4c_upload_yorc_plugin.bash [--a4c-url <Alien4Cloud URL>] [--yorc-plugin-url <Yorc URL>]"
+    echo "a4c_create_orchestrator.bash [--a4c-url <Alien4Cloud URL>] [--yorc-url <Yorc URL>]"
     echo "   - default A4C URL        : $a4cURL"
     echo "   - default Yorc plugin URL: $yorcPluginURL"
     exit 1
@@ -26,8 +28,8 @@ case $key in
     shift # past argument
     shift # past value
     ;;
-    -y|--yorc-plugin-url)
-    yorcPluginURL="$2"
+    -y|--yorc-url)
+    yorcURL="$2"
     shift # past argument
     shift # past value
     ;;
@@ -50,48 +52,32 @@ curl -d "username=admin&password=admin&submit=Login"  \
 	 --silent \
      --cookie-jar cookies.a4c
 
-# Check the plugin is not yet registered
+# Check if a Yorc orchestrator alreays exists
 curl --request GET \
-         --url $a4cURL/rest/latest/plugins?query=alien4cloud-yorc-plugin \
+         --url $a4cURL/rest/latest/orchestrators?query=Yorc \
          --header 'Accept: application/json' \
          --silent \
          --cookie cookies.a4c \
-| grep --quiet alien4cloud-yorc-plugin
+| grep --quiet Yorc
 
 res=$?
 if [ $res -eq 0 ]
     then
-        echo "Plugin already registered"
+        echo "Orchestrator already registered"
         exit 0
 fi
 
-# Download Yorc plugin
-echo "Downloading the plugin..."
-curl --request GET \
-         --url $yorcPluginURL \
-         --location \
-         --output yorcPlugin.zip
-
-res=$?
-if [ $res -ne 0 ]
-then
-    echo "Exiting on error downloading the plugin at $yorcPluginURL"
-    exit 1
-fi
-
+# Create the orchestrator
 curl --request POST \
-         --url $a4cURL/rest/latest/plugins \
-         --header 'Content-Type: multipart/form-data' \
+         --url $a4cURL/rest/latest/orchestrators \
+         --header 'Content-Type: application/json' \
          --cookie cookies.a4c \
          --silent \
-         --form 'file=@yorcPlugin.zip' > /dev/null
+          --data "{\"name\": \"Yorc\", \"pluginId\": \"alien4cloud-yorc-plugin\", \"pluginBean\": \"yorc-orchestrator-factory\"}" > /dev/null
 
 res=$?
 if [ $res -ne 0 ]
 then
-    echo "Exiting on error uploading the plugin in Alien4Cloud"
+    echo "Exiting on error creating the orchestrator in Alien4Cloud"
     exit 1
-else
-    echo "Plugin registered in Alien4Cloud"
 fi
-
